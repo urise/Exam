06ddec1d5e57875c37fa702f7da8a2fa5d95a507
Yuriy Dolzhikov <urise@i.ua>
2015-05-23 17:38:12 +0300

    Checking right order

--- js/main.js
+++ js/main.js
@@ -70,6 +70,7 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("id", ev.target.id);
+    ev.dataTransfer.setData("parentId", ev.target.parentNode.id);
 }
 
 function drop(ev) {
@@ -77,20 +78,52 @@ function drop(ev) {
     var id = ev.dataTransfer.getData("id");
     var li = document.getElementById(id);
     var list = ev.target.nodeName === "LI" ? ev.target.parentNode : ev.target;
-    var expectedOlId = getExpectedOlId(li);
     list.appendChild(li);
-
-    if (expectedOlId === list.id) {
-        setRightPosition(li);        
-    } else {
-        li.classList.remove("rightPosition");
-        li.classList.add("wrongPosition"); 
+    estimatePuzzleOl(list);
+    
+    var listFromId = ev.dataTransfer.getData("parentId");
+    if (listFromId !== "itemList") {
+        var listFrom = document.getElementById(listFromId);
+        estimatePuzzleOl(listFrom);
     }
 }
 
-function setRightPosition(li) {
+function deleteEstimationClasses(li) {
+    li.classList.remove("rightPosition");
     li.classList.remove("wrongPosition");
+    li.classList.remove("wrongOrderPosition");
+}
+
+function setRightPosition(li) {
+    deleteEstimationClasses(li);
     li.classList.add("rightPosition");
+}
+
+function setWrongPosition(li) {
+    deleteEstimationClasses(li);
+    li.classList.add("wrongPosition");
+}
+
+function setWrongOrderPosition(li) {
+    deleteEstimationClasses(li);
+    li.classList.add("wrongOrderPosition");
+}
+
+function estimatePuzzleOl(ol) {
+    for (var i = 0; i < ol.childNodes.length; i++) {
+        var li = ol.childNodes[i];
+        var expectedOlId = getExpectedOlId(li);
+
+        if (expectedOlId === ol.id) {
+            var liOrder = li.getAttribute("data-order");
+            if (liOrder == -1 || liOrder == i)
+                setRightPosition(li);        
+            else
+                setWrongOrderPosition(li);
+        } else {
+            setWrongPosition(li);
+        }
+    }
 }
 
 function clearPuzzle() {
@@ -113,6 +146,7 @@ function reset() {
 }
 
 function showCorrect() {
+    reset();
     clearLowerList();
     var lowerList = document.getElementById("itemList");
 
@@ -127,5 +161,4 @@ function showCorrect() {
             lowerList.appendChild(li);
 
 	}
-
 }
